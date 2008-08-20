e78526c96575d17f48e426c2b0bf6ef6c35fbc7b
chrisv <cvanderknyff@users.noreply.github.com>
2008-08-20 16:14:06 +0000

    FEATURE Added tracker2room and unit2sensor events to TrackerRemote.

--- VrpnNet/TrackerRemote.cpp
+++ VrpnNet/TrackerRemote.cpp
@@ -11,6 +11,9 @@ using namespace Vrpn;
 delegate void TrackerChangeCallback(void *userData, const vrpn_TRACKERCB info);
 delegate void TrackerVelocityCallback(void *userData, const vrpn_TRACKERVELCB info);
 delegate void TrackerAccelCallback(void *userData, const vrpn_TRACKERACCCB info);
+delegate void TrackerToRoomCallback(void *userData, const vrpn_TRACKERTRACKER2ROOMCB info);
+delegate void TrackerUnitToSensorCallback(void *userData, const vrpn_TRACKERUNIT2SENSORCB info);
+delegate void TrackerWorkspaceCallback(void *userData, const vrpn_TRACKERWORKSPACECB info);
 
 TrackerRemote::TrackerRemote(String ^name)
 {
@@ -43,6 +46,9 @@ TrackerRemote::~TrackerRemote()
 	gc_positionChange.Free();
 	gc_velocityChange.Free();
 	gc_accelChange.Free();
+	gc_t2rChange.Free();
+	gc_u2sChange.Free();
+	gc_boundsChange.Free();
 }
 
 void TrackerRemote::Update()
@@ -101,55 +107,106 @@ void TrackerRemote::RegisterHandlers()
 	TrackerChangeCallback ^poscb = gcnew TrackerChangeCallback(this, &TrackerRemote::onPositionChange);
 	gc_positionChange = GCHandle::Alloc(poscb);
 	IntPtr pPosChange = Marshal::GetFunctionPointerForDelegate(poscb);
-	vrpn_TRACKERCHANGEHANDLER pPosChangeHandler = static_cast<vrpn_TRACKERCHANGEHANDLER>(pPosChange.ToPointer());
+	vrpn_TRACKERCHANGEHANDLER pPosChangeHandler = 
+		static_cast<vrpn_TRACKERCHANGEHANDLER>(pPosChange.ToPointer());
 	m_tracker->register_change_handler(0, pPosChangeHandler);
 
 	// Register velocity handler
 	TrackerVelocityCallback ^velcb = gcnew TrackerVelocityCallback(this, &TrackerRemote::onVelocityChange);
 	gc_velocityChange = GCHandle::Alloc(velcb);
 	IntPtr pVelChange = Marshal::GetFunctionPointerForDelegate(velcb);
-	vrpn_TRACKERVELCHANGEHANDLER pVelChangeHandler = static_cast<vrpn_TRACKERVELCHANGEHANDLER>(pVelChange.ToPointer());
+	vrpn_TRACKERVELCHANGEHANDLER pVelChangeHandler = 
+		static_cast<vrpn_TRACKERVELCHANGEHANDLER>(pVelChange.ToPointer());
 	m_tracker->register_change_handler(0, pVelChangeHandler);
 
 	// Register acceleration handler
 	TrackerAccelCallback ^acccb = gcnew TrackerAccelCallback(this, &TrackerRemote::onAccelChange);
 	gc_accelChange = GCHandle::Alloc(acccb);
 	IntPtr pAccChange = Marshal::GetFunctionPointerForDelegate(acccb);
-	vrpn_TRACKERACCCHANGEHANDLER pAccChangeHandler = static_cast<vrpn_TRACKERACCCHANGEHANDLER>(pAccChange.ToPointer());
+	vrpn_TRACKERACCCHANGEHANDLER pAccChangeHandler = 
+		static_cast<vrpn_TRACKERACCCHANGEHANDLER>(pAccChange.ToPointer());
 	m_tracker->register_change_handler(0, pAccChangeHandler);
+
+	// Register tracker-to-room handler
+	TrackerToRoomCallback ^t2rcb = gcnew TrackerToRoomCallback(this, &TrackerRemote::onT2RChange);
+	gc_t2rChange = GCHandle::Alloc(t2rcb);
+	IntPtr pT2RChange = Marshal::GetFunctionPointerForDelegate(t2rcb);
+	vrpn_TRACKERTRACKER2ROOMCHANGEHANDLER pT2RChangeHandler = 
+		static_cast<vrpn_TRACKERTRACKER2ROOMCHANGEHANDLER>(pT2RChange.ToPointer());
+	m_tracker->register_change_handler(0, pT2RChangeHandler);
+
+	// Register unit-to-sensor handler
+	TrackerUnitToSensorCallback ^u2scb = gcnew TrackerUnitToSensorCallback(this, &TrackerRemote::onU2SChange);
+	gc_u2sChange = GCHandle::Alloc(u2scb);
+	IntPtr pU2SChange = Marshal::GetFunctionPointerForDelegate(u2scb);
+	vrpn_TRACKERUNIT2SENSORCHANGEHANDLER pU2SChangeHandler =
+		static_cast<vrpn_TRACKERUNIT2SENSORCHANGEHANDLER>(pU2SChange.ToPointer());
+	m_tracker->register_change_handler(0, pU2SChangeHandler);
+
+	// Register workspace handler
+	TrackerWorkspaceCallback ^wcb = gcnew TrackerWorkspaceCallback(this, &TrackerRemote::onBoundsChange);
+	gc_boundsChange = GCHandle::Alloc(wcb);
+	IntPtr pBoundsChange = Marshal::GetFunctionPointerForDelegate(wcb);
+	vrpn_TRACKERWORKSPACECHANGEHANDLER pBoundsHandler = 
+		static_cast<vrpn_TRACKERWORKSPACECHANGEHANDLER>(pBoundsChange.ToPointer());
+	m_tracker->register_change_handler(0, pBoundsHandler);
 }
 
-void TrackerRemote::onPositionChange(void *userData, const vrpn_TRACKERCB info)
+void TrackerRemote::onPositionChange(void *, const vrpn_TRACKERCB info)
 {
 	TrackerChangeEventArgs ^e = gcnew TrackerChangeEventArgs();
 	e->Time = VrpnUtils::ConvertTimeval(info.msg_time);
 	e->Sensor = info.sensor;
 	e->Position = Vector3(info.pos[0], info.pos[1], info.pos[2]);
-	e->Orientation = Quaternion(info.quat[Q_X], info.quat[Q_Y], info.quat[Q_Z], info.quat[Q_W]);
+	e->Orientation = Quaternion(info.quat[Q_X], info.quat[Q_Y], 
+		info.quat[Q_Z], info.quat[Q_W]);
 
 	PositionChanged(this, e);
 }
 
-void TrackerRemote::onVelocityChange(void *userData, const vrpn_TRACKERVELCB info)
+void TrackerRemote::onVelocityChange(void *, const vrpn_TRACKERVELCB info)
 {
 	TrackerVelocityChangeEventArgs ^e = gcnew TrackerVelocityChangeEventArgs();
 	e->Time = VrpnUtils::ConvertTimeval(info.msg_time);
 	e->Sensor = info.sensor;
 	e->Velocity = Vector3(info.vel[0], info.vel[1], info.vel[2]);
-	e->VelocityQuat = Quaternion(info.vel_quat[Q_X], info.vel_quat[Q_Y], info.vel_quat[Q_Z], info.vel_quat[Q_W]);
+	e->VelocityQuat = Quaternion(info.vel_quat[Q_X], info.vel_quat[Q_Y], 
+		info.vel_quat[Q_Z], info.vel_quat[Q_W]);
 	e->VelocityQuatDeltaSeconds = info.vel_quat_dt;
 
 	VelocityChanged(this, e);
 }
 
-void TrackerRemote::onAccelChange(void *userData, const vrpn_TRACKERACCCB info)
+void TrackerRemote::onAccelChange(void *, const vrpn_TRACKERACCCB info)
 {
 	TrackerAccelChangeEventArgs ^e = gcnew TrackerAccelChangeEventArgs();
 	e->Time = VrpnUtils::ConvertTimeval(info.msg_time);
 	e->Sensor = info.sensor;
 	e->Acceleration = Vector3(info.acc[0], info.acc[1], info.acc[2]);
-	e->AccelerationQuat = Quaternion(info.acc_quat[Q_X], info.acc_quat[Q_Y], info.acc_quat[Q_Z], info.acc_quat[Q_W]);
+	e->AccelerationQuat = Quaternion(info.acc_quat[Q_X], info.acc_quat[Q_Y], 
+		info.acc_quat[Q_Z], info.acc_quat[Q_W]);
 	e->AccelerationQuatDeltaSeconds = info.acc_quat_dt;
 
 	AccelerationChanged(this, e);
+}
+
+void TrackerRemote::onT2RChange(void *, const vrpn_TRACKERTRACKER2ROOMCB info)
+{
+	TrackerToRoomEventArgs ^e = gcnew TrackerToRoomEventArgs();
+	e->Time = VrpnUtils::ConvertTimeval(info.msg_time);
+	e->Position = Vector3(info.tracker2room[0], info.tracker2room[1], info.tracker2room[2]);
+	e->Orientation = Quaternion(info.tracker2room_quat[Q_X], info.tracker2room_quat[Q_Y],
+		info.tracker2room_quat[Q_Z], info.tracker2room_quat[Q_W]);
+}
+
+void TrackerRemote::onU2SChange(void *, const vrpn_TRACKERUNIT2SENSORCB info)
+{
+	TrackerUnitToSensorEventArgs ^e = gcnew TrackerUnitToSensorEventArgs();
+	e->Time = VrpnUtils::ConvertTimeval(info.msg_time);
+	e->Sensor = info.sensor;
+	e->Position = Vector3(info.unit2sensor[0], info.unit2sensor[1], info.unit2sensor[2]);
+	e->Orientation = Quaternion(info.unit2sensor_quat[Q_X], info.unit2sensor_quat[Q_Y], 
+		info.unit2sensor_quat[Q_Z], info.unit2sensor_quat[Q_W]);
+
+	UnitToSensorChanged(this, e);
 }
