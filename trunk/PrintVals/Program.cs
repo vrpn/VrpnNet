339c411ee6c5bb8aca073c87711a46673bd06530
chrisv <chrisv@919a4b7e-da40-4ba5-a668-eab891b48070>
2008-08-20 20:01:11 +0000

    FEATURE Completed TrackerRemote. Added complete ButtonRemote.
    Changed sample program to test UNC tracker.

--- trunk/PrintVals/Program.cs
+++ trunk/PrintVals/Program.cs
@@ -9,18 +9,28 @@ namespace PrintVals
     {
         static void PositionChanged(object sender, TrackerChangeEventArgs e)
         {
-            Console.WriteLine("{0} {1} {2}", e.Time, e.Sensor, e.Position);
+            Console.WriteLine("{0} {1} {2} {3} {4}", e.Time, e.Sensor, e.Position.X, e.Position.Y, e.Position.Z);
+        }
+
+        static void ButtonChanged(object sender, ButtonChangeEventArgs e)
+        {
+            Console.WriteLine("{0} {1} {2}", e.Time, e.Button, e.IsPressed ? "pressed" : "released");
         }
 
         static void Main(string[] args)
         {
-            TrackerRemote tracker = new TrackerRemote("Tracker0@localhost");
+            TrackerRemote tracker = new TrackerRemote("Tracker0@tracker1-cs.cs.unc.edu");
             tracker.PositionChanged += new TrackerChangeEventHandler(PositionChanged);
-            tracker.MuteWarnings = true;
+            tracker.MuteWarnings = false;
+
+            ButtonRemote button = new ButtonRemote("Button0@tracker1-cs.cs.unc.edu");
+            button.ButtonChanged += new ButtonChangeEventHandler(ButtonChanged);
+            button.MuteWarnings = true;
 
             while (true)
             {
                 tracker.Update();
+                button.Update();
             }
         }
     }
---
+++ trunk/VrpnNet/ButtonRemote.h
@@ -0,0 +1,45 @@
+#include "stdafx.h"
+
+#include "vrpn_Button.h"
+
+#include "BaseTypes.h"
+#include "Connection.h"
+
+namespace Vrpn {
+	public ref class ButtonChangeEventArgs: public System::EventArgs
+	{
+	public:
+		property System::DateTime Time;
+		property System::Int32 Button;
+		property System::Boolean IsPressed;
+	};
+
+	public delegate void ButtonChangeEventHandler(System::Object ^sender,
+		ButtonChangeEventArgs ^e);
+
+	public ref class ButtonRemote: public Vrpn::IVrpnObject
+	{
+	public:
+		ButtonRemote(System::String ^name);
+		ButtonRemote(System::String ^name, Vrpn::Connection ^connection);
+		~ButtonRemote();
+
+		virtual void Update(); // from IVrpnObject
+		virtual Connection^ GetConnection(); // from IVrpnObject
+		property System::Boolean MuteWarnings // from IVrpnObject
+		{
+			virtual void set(System::Boolean);
+			virtual System::Boolean get();
+		}
+
+		event ButtonChangeEventHandler^ ButtonChanged;
+
+	private:
+		::vrpn_Button_Remote *m_button;
+
+		void RegisterHandler();
+		void onButtonChange(void *userData, vrpn_BUTTONCB info);
+
+		System::Runtime::InteropServices::GCHandle gc_callback;
+	};
+}
