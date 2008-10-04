788d909e8a27db8bb86acb966471d57c2e3e841b
chrisv <cvanderknyff@users.noreply.github.com>
2008-10-04 00:37:56 +0000

    MAINTENANCE
    Preparing release as version 1.0.2.

--- VrpnNet/AssemblyInfo.cpp
+++ VrpnNet/AssemblyInfo.cpp
@@ -1,17 +1,17 @@
 // AssemblyInfo.cpp: VrpnNet assembly metadata.
 //
 // Copyright (c) 2008 Chris VanderKnyff
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -61,9 +61,9 @@ using namespace System::Security::Permissions;
 //
 // Revision is set to *, so it changes with every build.
 //
-// 
+//
 
-[assembly:AssemblyVersionAttribute("1.0.1.*")];
+[assembly:AssemblyVersionAttribute("1.0.2.*")];
 
 [assembly:ComVisible(false)];
 
