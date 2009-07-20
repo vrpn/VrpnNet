5d5c846550d9c89083c1d3f9614db7d9ccb7ab52
chrisv <cvanderknyff@users.noreply.github.com>
2009-07-20 17:42:20 +0000

    RELEASE 1.1.1 Updated changelog and assembly metadata with new version. Removed Visual Studio 2005 projects.

--- VrpnNet/AssemblyInfo.cpp
+++ VrpnNet/AssemblyInfo.cpp
@@ -63,7 +63,7 @@ using namespace System::Security::Permissions;
 //
 //
 
-[assembly:AssemblyVersionAttribute("1.1.0.*")];
+[assembly:AssemblyVersionAttribute("1.1.1.*")];
 
 [assembly:ComVisible(false)];
 
